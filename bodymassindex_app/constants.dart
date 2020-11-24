0d532ee0d494ca57a64c5555167594de01cadccf
mikaelpac <63595379+mikaelpac@users.noreply.github.com>
2020-11-24 12:45:24 +0200

    Add files via upload

---
+++ bodymassindex_app/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const kTitleTextStyle = TextStyle(fontWeight: FontWeight.bold);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kResultTitleStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22,
+);
+
+const kBottomContainerHeight = 60.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveColour = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
